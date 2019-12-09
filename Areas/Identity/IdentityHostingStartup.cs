20fd231924dd17bda99d3d0fb6e5bc12d99b67a1
Carl Barringer <cbarringer1@gmail.com>
2019-12-09 13:36:13 -0600

    Initial Commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BudgetMaster.Data;
+using BudgetMaster.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BudgetMaster.Areas.Identity.IdentityHostingStartup))]
+namespace BudgetMaster.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
