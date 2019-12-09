58571eec081bd39e02c88cb40175af1eb7a30826
Carl Barringer <cbarringer1@gmail.com>
2019-12-09 14:07:34 -0600

    added budget model

---
+++ Models/Budget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetMaster.Models
+{
+    public class Budget
+    {
+        [Key]
+        public int BudgetId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public ApplicationUser User { get; set; }
+        [Required]
+        public int CreatedMonth { get; set; }
+        [Required]
+        public int CreatedYear { get; set; }
+
+
+    }
+}
