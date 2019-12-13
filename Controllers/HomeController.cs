afe66323170028ced5689f840d8e62c06d47313e
Carl Barringer <cbarringer1@gmail.com>
2019-12-13 15:08:51 -0600

    expression working for actual expenses

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,22 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BudgetMaster.Models;
+using BudgetMaster.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BudgetMaster.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<HomeController> logger)
         {
+            _context = context;
+            _userManager = userManager;
             _logger = logger;
         }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await GetCurrentUserAsync();
+            var userBudget = await _context.Budgets
+                .Include(b => b.ActualExpenses)
+                //.Where(b => b.UserId == user.Id && b.BudgetId == 1)
+                .FirstOrDefaultAsync(b => b.BudgetId == 2);
+            return View(userBudget);
         }
+
 
         public IActionResult Privacy()
         {
--- Models/Budget.cs
+++ Models/Budget.cs
@@ -20,5 +20,15 @@ namespace BudgetMaster.Models
         public int CreatedYear { get; set; }
 
 
+        //Info for Projected Income
+        //public ICollection<ProjectedIncome> ProjectedIncomes { get; set; }
+
+        //Infor for Projected Expense
+        //public ICollection<ProjectedIncome> ProjectedExpenses { get; set; }
+        //Infor for Actual Income
+        //public ICollection<ActualIncome> ActualIncomes { get; set; }
+        //Info for Actual Expense
+        public ICollection<ActualExpense> ActualExpenses { get; set; }
+
     }
 }
